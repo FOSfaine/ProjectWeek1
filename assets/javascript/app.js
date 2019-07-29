e73d25bcf441e154600892b13f744ad7065127ef
Esther Cho <Eunsem.64642@gmail.com>
2019-07-28 21:19:24 -0600

    Firebase config code added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function (window) {
     console.log("document ready");
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCJF2Rjt64-Xubs-mipH-tF42L4_Vz9R0Y",
+    authDomain: "project1-579e0.firebaseapp.com",
+    databaseURL: "https://project1-579e0.firebaseio.com",
+    projectId: "project1-579e0",
+    storageBucket: "project1-579e0.appspot.com",
+    messagingSenderId: "861935562453",
+    appId: "1:861935562453:web:935185dce5c6989b"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
 })
