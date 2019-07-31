4c77ca207b450cf149460f149fa0b16e59afaf63
Esther Cho <Eunsem.64642@gmail.com>
2019-07-30 21:02:25 -0600

    NYT API starter-code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -56,4 +56,38 @@ req.end(function (res) {
 	console.log(res.body);
 })
 
+
+// This is the NYT API search, but it has search input parameters built-in (search-term and start/end dates).
+// We probably don't want those on the HTML, but will want to add parameters to our API call for the crupto-news sidebar.
+
+function buildQueryURL() {
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+  var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
+
+  queryParams.q = $("#search-term")
+    .val()
+    .trim();
+
+  var startYear = $("#start-year")
+    .val()
+    .trim();
+
+  if (parseInt(startYear)) {
+    queryParams.begin_date = startYear + "0101";
+  }
+
+  var endYear = $("#end-year")
+    .val()
+    .trim();
+
+  if (parseInt(endYear)) {
+    queryParams.end_date = endYear + "0101";
+  }
+
+  console.log("---------------\nURL: " + queryURL + "\n---------------");
+  console.log(queryURL + $.param(queryParams));
+  return queryURL + $.param(queryParams);
+}
+
 })
