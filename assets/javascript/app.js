0957625f6f8cb6306c3f396e7083230050926540
Esther Cho <Eunsem.64642@gmail.com>
2019-07-30 20:34:35 -0600

    added initial API codes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ $(document).ready(function (window) {
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  
+
 
 // CoinAPI
 var unirest = require("unirest");
@@ -32,9 +32,6 @@ req.end(function (res) {
 	console.log(res.body);
 });
 
-
-// CoinMarketCap: keeps stalling, coming back with 504 error code 
-// (server did not recieve a timely response)
 
 // Currency Exchange
 var unirest = require("unirest");
@@ -58,43 +55,5 @@ req.end(function (res) {
 
 	console.log(res.body);
 })
-
-// Reddit
-appClientID: SWNU-Lo2AJ4K8NM6SCch-XIPr1w
-
-// access token:
-https://www.reddit.com/api/v1/authorize?client_id=SWNU-Lo2AJ4K8NM6SCch-XIPr1w&response_type=code&state=ProjectWeek1&redirect_uri=https://estherecho.github.io/ProjectWeek1/&duration=temporary&scope=mysubreddits
-  
-
-// The response from this request, if successful, will be JSON of the following format:
-{
-  "access_token": Your access token,
-  "token_type": "bearer",
-  "expires_in": Unix Epoch Seconds,
-  "scope": A scope string,
-}
-
-// reddit continued: This did not exactly work, threw an error... I'm pretty sure it has to do with the access token (syntax error)
-var unirest = require("unirest");
-
-var req = unirest("POST", "https://redditdimashirokovv1.p.rapidapi.com/aboutSubreddit");
-
-req.headers({
-	"x-rapidapi-host": "RedditdimashirokovV1.p.rapidapi.com",
-	"x-rapidapi-key": "8a398d0356msh92b4964ad1b331dp1482c2jsna497f038258b",
-	"content-type": "application/x-www-form-urlencoded"
-});
-
-req.form({
-	"appClientId": {},
-	"subreddit": {},
-	"accessToken": {}
-});
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
 
 })
