aad8d8cf8470c49758075c98620aa60476996fa7
Colin D <colinv120@gmail.com>
2019-08-01 18:43:19 -0600

    new auth comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,87 +1,104 @@
 $(document).ready(function (window) {
-  console.log("document ready");
+      console.log("document ready");
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyCJF2Rjt64-Xubs-mipH-tF42L4_Vz9R0Y",
-    authDomain: "project1-579e0.firebaseapp.com",
-    databaseURL: "https://project1-579e0.firebaseio.com",
-    projectId: "project1-579e0",
-    storageBucket: "project1-579e0.appspot.com",
-    messagingSenderId: "861935562453",
-    appId: "1:861935562453:web:935185dce5c6989b"
-  };
+      var firebaseConfig = {
+        apiKey: "AIzaSyCJF2Rjt64-Xubs-mipH-tF42L4_Vz9R0Y",
+        authDomain: "project1-579e0.firebaseapp.com",
+        databaseURL: "https://project1-579e0.firebaseio.com",
+        projectId: "project1-579e0",
+        storageBucket: "project1-579e0.appspot.com",
+        messagingSenderId: "861935562453",
+        appId: "1:861935562453:web:935185dce5c6989b"
+      };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      var database = firebase.database();
 
-  // CoinAPI
-  var unirest = require("unirest");
+      // CoinAPI
+      var unirest = require("unirest");
 
-  var req = unirest("GET", "https://coinapi.p.rapidapi.com/v1/exchangerate/USD");
+      var req = unirest("GET", "https://coinapi.p.rapidapi.com/v1/exchangerate/USD");
 
-  req.headers({
-    "x-rapidapi-host": "coinapi.p.rapidapi.com",
-    "x-rapidapi-key": "8a398d0356msh92b4964ad1b331dp1482c2jsna497f038258b"
-  });
-
-
-
-  req.end(function (res) {
-    if (res.error) throw new Error(res.error);
-
-    console.log(res.body);
-  });
+      req.headers({
+        "x-rapidapi-host": "coinapi.p.rapidapi.com",
+        "x-rapidapi-key": "8a398d0356msh92b4964ad1b331dp1482c2jsna497f038258b"
+      });
 
 
-  // Currency Exchange
-  var unirest = require("unirest");
 
-  var req = unirest("GET", "https://currency-exchange.p.rapidapi.com/exchange");
+      req.end(function (res) {
+        if (res.error) throw new Error(res.error);
 
-  req.query({
-    "q": "1.0",
-    "from": "USD",
-    "to": "GBP"
-  });
-
-  req.headers({
-    "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
-    "x-rapidapi-key": "8a398d0356msh92b4964ad1b331dp1482c2jsna497f038258b"
-  });
+        console.log(res.body);
+      });
 
 
-  req.end(function (res) {
-    if (res.error) throw new Error(res.error);
+      // Currency Exchange
+      var unirest = require("unirest");
 
-    console.log(res.body);
-  })
+      var req = unirest("GET", "https://currency-exchange.p.rapidapi.com/exchange");
 
-  // Firebase Auth
+      req.query({
+        "q": "1.0",
+        "from": "USD",
+        "to": "GBP"
+      });
+
+      req.headers({
+        "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+        "x-rapidapi-key": "8a398d0356msh92b4964ad1b331dp1482c2jsna497f038258b"
+      });
 
 
-  // New User 
-  // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  //   // Handle Errors here.
-  //   var errorCode = error.code;
-  //   var errorMessage = error.message;
-  //   // ...
-  // });
+      req.end(function (res) {
+        if (res.error) throw new Error(res.error);
 
-  // Sign-In
-  // var email = $("#emailInput").val()
-  // var password = $("#emailInput").val()
-  // var runAuth = firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  //   // Handle Errors here.
-  //   var errorCode = error.code;
-  //   var errorMessage = error.message;
-  //   // ...
-  // });
+        console.log(res.body);
+      })
 
-  // User SignOut
-  // firebase.auth().signOut().then(function() {
-  //   // Sign-out successful.
-  // }).catch(function(error) {
-  //   // An error happened.
-  // });
-})
+      // Firebase Auth
+
+
+      // // New User
+      // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+      //   // Handle Errors here.
+      //   var errorCode = error.code;
+      //   var errorMessage = error.message;
+      //   // ...
+      // });
+
+      // // Sign - In
+      // var userEmail = $("#emailInput").val()
+      // var userPassword = $("#emailInput").val()
+      // var runAuth = firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+      //   // Handle Errors here.
+      //   var errorCode = error.code;
+      //   var errorMessage = error.message;
+      //   // ...
+      // });
+
+      // // User SignOut
+      // firebase.auth().signOut().then(function () {
+      //   // Sign-out successful.
+      // }).catch(function (error) {
+      //   // An error happened.
+      // });
+
+      // firebase.auth().onAuthStateChanged(function (user) {
+      //   if (user) {
+      //     // User is signed in.
+      //   } else {
+      //     // No user is signed in.
+      //   }
+      // });
+
+      // var user = firebase.auth().currentUser;
+      // var name, email, photoUrl, uid, emailVerified;
+
+      // if (user != null) {
+      //   name = user.displayName;
+      //   email = user.email;
+      //   photoUrl = user.photoURL;
+      //   emailVerified = user.emailVerified;
+      // })
