e4265af17b6351d82d9118a292f892a7be0d24e0
Broc Atkinson <44753400+BrocAtkinson@users.noreply.github.com>
2019-08-01 21:15:16 -0600

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,1 @@
-function news() {
-  var news = $(this).attr("data-name");
-  var queryURL =
-    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=cryptocurrency&api-key=F86BqhNAvBubVGR0OjFhBa8R1QbGr3gD";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    $("#news-view").text(JSON.stringify(response));
-  });
-}
